19ade58205d3157c32f6fdaca70358d5a1d2d4ee
J. Francisco Martín <jfm.lisaso@gmail.com>
2019-01-02 03:01:45 +0100

    Corrección de mensajes en las extensiones de mensajes de la librería

--- Extensions/sp3present.h
+++ Extensions/sp3present.h
@@ -931,7 +931,7 @@
 				else print "podría llevarse";
 				" aquello.";
 			11:
-				"", (The) x1, "está", (n) x1, " fij", (o) x1,
+				"", (The) x1, " está", (n) x1, " fij", (o) x1,
 				" en el sitio.";
 			12:
 				print "Ya ";
