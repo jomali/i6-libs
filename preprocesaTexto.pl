d2090f8636610153cbd0a82c018fcf5f8d663604
J. Francisco Martín <jfm.lisaso@gmail.com>
2019-02-22 21:06:11 +0100

    Fichero de configuración por defecto para garglk. Actualización del script de preprocesado

--- preprocesaTexto.pl
+++ preprocesaTexto.pl
@@ -1,17 +1,27 @@
 #!/usr/bin/perl
 
-#	File:			preprocesaTexto.pl
-#	Author(s):		J. Francisco Martín <jfm.lisaso@gmail.com>
-#	Version:		2.1
-#	Released:		2018/10/09
+#	File:		preprocesaTexto.pl
+#	Author(s):	J. Francisco Martín <jfm.lisaso@gmail.com>
+#	Version:	3.0
+#	Released:	2018/12/24
 #
-#	Script Perl para preprocesar un archivo de texto. Sustituye ciertas
-#	etiquetas de texto por funciones definidas dentro del relato para manipular
-#	el estilo de los textos, listar objetos contenidos por otro objeto, o crear
-#	hipervínculos, por ejemplo.
+#	Script Perl para preprocesar código fuente escrito en lenguaje Inform 6.
+#	Permite añadir ciertas etiquetas a las descripciones de los objetos, que
+#	tras el preprocesado son sustituidas por funciones para manipular el
+#	estilo de los textos, crear sentencias condicionales, listar objetos
+#	contenidos por otro objeto o crear hipervínculos, por ejemplo.
 #
-#	HISTORIAL DE VERSIONES:
+#	HISTORIAL DE VERSIONES
 #
+#	3.1: 2019/02/20	El nombre de la rutina encargada de crear hipervínculos se
+#					traslada a la variable $hyperlinks_routine para facilitar
+#					su modificación
+#	3.0: 2018/12/24	Se redefinen las expresiones regulares para permitir
+#					ejeutar el preprocesado sobre cualquier fichero con código
+#					fuente escrito en Inform 6. Se añaden etiquetas para
+#					mostrar texto condicional en función del número del objeto
+#					controlado por el usuario y de la hora del día del modelo
+#					de mundo.
 #	2.1: 2018/10/09	Se utiliza una nueva rutina para crear hipervínculos
 #	2.0: 2017/09/11	Redefinición de las expresiones regulares.
 #	1.1: 2016/06/18	Añade etiquetas para crear hipervínuclos con distintas
@@ -19,6 +29,8 @@
 #	1.0: 2014/09/20 Incluye las etiquetas básicas para cambiar estilos de
 #					texto, listar objetos contenidos dentro de otro objeto, y
 #					crear hipervínculos.
+
+$hyperlinks_routine = "PRT__";
 
 if ($#ARGV != 1) {
 	print "Se deben especificar los archivos de entrada y salida del script.";
@@ -39,49 +51,73 @@ close FILE;
 open (STDOUT, ">$output_file")
 	or die "No se pudo abrir el archivo de salida $output_file: $!\n";
 
-# Se hacen las sustituciones (EL ORDEN EN QUE SE HACEN IMPORTA)
+# Sustituciones (EL ORDEN EN QUE SE HACEN IMPORTA):
 for (@lines) {
 
-	# Omite las líneas de comentario (aquellas que empiezan por '!' o '#'):
-	next if /^\s*(!|#)/;
-
-	# Imprime los caracteres '[' y ']':
+	# Caracteres '[' y ']':
 	s/\\\[/", (char) 91, "/g;
 	s/\\\]/", (char) 93, "/g;
 
-	# Transforma las etiquetas '**' en estilo fuerte:
+	# Etiquetas para estilo fuerte: **texto**
 	s/(?<!\\)\*{2}([^\*\n]+)(?<!\\)\*{2}/", (strong) "\1", "/g;
-	# Transforma las etiquetas '*' en estilo enfatizado:
+	# Etiquetas para el estilo enfatizado: *texto*
 	s/(?<!\\)\*([^\*\n]+)(?<!\\)\*/", (emph) "\1", "/g;
-	# Transforma las etiquetas '`' en estilo de tipo código:
-	s/(?<!\\)`([^`\n]+)(?<!\\)`/", (code) "\1", "/g;
+	# Etiquetas para el estilo código: `texto`
+	s/(?<!\\)`([^`\n]+)(?<!\\)`/", (monospaced) "\1", "/g;
 
-	# Imprime el nombre corto del objeto con el artículo determinado adecuado:
+	# Secuencia condicional si el objeto player es plural: [plural:player]
+	s/\[\s*plural:player\s*\]/";\nif (IsPluralNoun(player)) {\nprint "/g;
+	# Secuencia condicional si es de día: [if:dia]
+	s/\[\s*if:dia\s*\]/";\nif (Utils.get_current_hour()>7 && Utils.get_current_hour()<21) {\nprint "/g;
+	# Secuencia condicional si el modo brújula está activado: [if:compass_mode]
+	s/\[\s*if:compass_mode\s*\]/";\nif (Utils.is_compass_enabled()) {\nprint "/g;
+	# Secuencia condicional: [else]
+	s/\[\s*else\s*\]/";\n} else {\nprint "/g;
+	# Secuencia condicional: [fi]
+	s/\[\s*fi\s*\]/";\n}\nprint "/g;
+
+	# Nombre corto del objeto junto al artículo determinado adecuado:
+	# [el/la/los/las objeto]
 	s/\[\s*(el|la|los|las)\s+(.+?)\s*\]/", (the) \2, "/g;
+	# [El/La/Los/Las objeto]
 	s/\[\s*(El|La|Los|Las)\s+(.+?)\s*\]/", (The) \2, "/g;
-	# Ídem, considerando las contracciones 'al' y 'del':
+	# [al/a la/a los/a las objeto]
 	s/\[\s*(al|a\s+la|a\s+los|a\s+las)\s+(.+?)\s*\]/", (al) \2, "/g;
+	# [Al/A la/A los/A las objeto]
 	s/\[\s*(Al|A\s+la|A\s+los|A\s+las)\s+(.+?)\s*\]/", (_Al) \2, "/g;
+	# [del/de la/de los/de las objeto]
 	s/\[\s*(del|de\s+la|de\s+los|de\s+las)\s+(.+?)\s*\]/", (del) \2, "/g;
+	# [Del/De la/De los/De las objeto]
 	s/\[\s*(Del|De\s+la|De\s+los|De\s+las)\s+(.+?)\s*\]/", (_Del) \2, "/g;
-	# Imprime el nombre corto del objeto con el artículo indeterminado adecuado:
+
+	# Nombre corto del objeto junto al artículo indeterminado adecuado:
+	# [un/una/unos/unas objeto]
 	s/\[\s*(un|una|unos|unas)\s+(.+?)\s*\]/", (a) \2, "/g;
+	# [Un/Una/Unos/Unas objeto]
 	s/\[\s*(Un|Una|Unos|Unas)\s+(.+?)\s*\]/", (A) \2, "/g;
 
-	# Imprime la terminación (n/o) adecuada:
+	# Terminación de número adecuada: [n obj]
 	s/\[\s*n\s+(.+?)\s*\]/", (n) \1, "/g;
+	# Terminación de género adeuada: [o obj]
 	s/\[\s*o\s+(.+?)\s*\]/", (o) \1, "/g;
 
-	# Imprime lista de objetos contenidos en otro objeto:
+	# Lista de objetos contenidos por otro objeto:
+	# [lista de objetos en/sobre obj<códigos de listado>]
 	s/\[\s*lista\s+de\s+objetos\s+(en|sobre)\s+(.+?)\s*\<\s*(.+?)\s*\>\s*\]/";\nWriteListFrom(child(\2), \3);\nprint "/g;
+	# [lista de objetos en/sobre obj]
 	s/\[\s*lista\s+de\s+objetos\s+(en|sobre)\s+(.+?)\s*\]/";\nWriteListFrom(child(\2), ENGLISH_BIT);\nprint "/g;
 
-	# Hipervínculo asociado a un objeto, con un texto alternativo:
-	s/(?<!\\)\[([^\[\]]+)(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\:\s*([^\(\)\:\s]+)(?<!\\)\)/";\nUtil.extended_print(\1, "\2", \3);\nprint "/g;
-	s/(?<!\\)\[([^\[\]]+)(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\)/";\nUtil.extended_print(\1, "\2", -1);\nprint "/g;
-	# Hipervínculo creado a partir de un texto:
-	s/(?<!\\)\[(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\:\s*([^\(\)\:\s]+)(?<!\\)\)/";\nUtil.extended_print("\1", "\1", \2);\nprint "/g;
-	s/(?<!\\)\[(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)(?<!\\)\)/";\nUtil.extended_print("\1", "\1", -1);\nprint "/g;
+	# Hipervínculo asociado a un objeto, con texto alternativo:
+	# [obj](texto:código_estilo)
+	s/(?<!\\)\[([^\[\]]+)(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\:\s*([^\(\)\:\s]+)(?<!\\)\)/";\n$hyperlinks_routine(\1, "\2", \3);\nprint "/g;
+	# [obj](texto)
+	s/(?<!\\)\[([^\[\]]+)(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\)/";\n$hyperlinks_routine(\1, "\2");\nprint "/g;
+
+	# Hipervínculo asociado a un texto:
+	# [](texto:código_estilo)
+	s/(?<!\\)\[(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)\:\s*([^\(\)\:\s]+)(?<!\\)\)/";\n$hyperlinks_routine("\1", "\1", \2);\nprint "/g;
+	# [](texto)
+	s/(?<!\\)\[(?<!\\)\](?<!\\)\(([^\(\)\:]+)(?<!\\)(?<!\\)\)/";\n$hyperlinks_routine("\1", "\1");\nprint "/g;
 
 	# Imprime el nombre corto del objeto:
 	s/\[\s*(.+?)\s*\]/", (name) \1, "/g;
